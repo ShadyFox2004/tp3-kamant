ecd568b4198726ebbbf52079c89716cb89683a99
DevTeamVersionControl <95041558+kcnqwe@users.noreply.github.com>
2022-11-14 14:54:30 -0500

    Skeleton for simulation service and javadoc is complete

--- src/main/java/a22/sim203/tp3/simulation/SimulationService.java
+++ src/main/java/a22/sim203/tp3/simulation/SimulationService.java
@@ -1,9 +1,14 @@
 package a22.sim203.tp3.simulation;
 
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import org.mariuszgromada.math.mxparser.Argument;
 import org.mariuszgromada.math.mxparser.Function;
 
+import java.security.Provider;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -12,10 +17,89 @@ import static java.lang.Thread.interrupted;
 
 /**
  * Makes the service run on an independent state
+ * @author Kamran Charles Nayebi
  */
-public class SimulationService  {
+public class SimulationService extends Service<Void> {
 
     private Simulation simulation;
+
+    private long absoluteStartTime;
+
+    private double targetDeltaTime;
+
+    private boolean paused;
+
+    /**
+     * Creates a service that runs a simulation
+     * @param simulation the simulation to run
+     * @param targetDeltaTime the target for the time in between simulation steps
+     * @param paused states if the simulation is running
+     */
+    SimulationService(Simulation simulation, double targetDeltaTime, boolean paused){
+        setSimulation(simulation);
+        setTargetDeltaTime(targetDeltaTime);
+        setPaused(paused);
+    }
+
+    /**
+     * Creates a simulation task
+     * @return a simulation task
+     */
+    @Override
+    protected Task<Void> createTask() {
+        setAbsoluteStartTime(System.currentTimeMillis());
+        return new simulationTask();
+    }
+
+    /**
+     * Task that creates a new state from another state
+     */
+    private class simulationTask extends Task<Void> {
+
+        @Override
+        protected Void call() throws Exception {
+            return null;
+        }
+    }
+
+    public Simulation getSimulation() {
+        return simulation;
+    }
+
+    public void setSimulation(Simulation simulation) {
+        this.simulation = simulation;
+    }
+
+    public long getAbsoluteStartTime() {
+        return absoluteStartTime;
+    }
+
+    /**
+     * Sets the absolute time since the start of the simulation
+     * @warning This can very easily break things, use only when there is no other way to do things
+     * @param absoluteStartTime the time in milliseconds since January 1, 1970, 00:00:00 GTM when the simulation is first run
+     *      *                          (standard time stored on the motherboard, can be had by calling System.currentTimeMillis())
+     */
+    public void setAbsoluteStartTime(long absoluteStartTime) {
+        this.absoluteStartTime = absoluteStartTime;
+    }
+
+    public double getTargetDeltaTime() {
+        return targetDeltaTime;
+    }
+
+    public void setTargetDeltaTime(double targetDeltaTime) {
+        this.targetDeltaTime = targetDeltaTime;
+    }
+
+    public boolean isPaused() {
+        return paused;
+    }
+
+
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
 
 
 
