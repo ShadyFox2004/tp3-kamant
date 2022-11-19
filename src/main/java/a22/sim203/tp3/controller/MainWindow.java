ddcca420d9a948b1a9122627ea66294412d9c7fe
ShadyFox2004 <amboudreau.amb@gmail.com>
2022-11-18 22:13:33 -0500

    added the main window controller

---
+++ src/main/java/a22/sim203/tp3/controller/MainWindow.java
@@ -0,0 +1,24 @@
+package controller;
+
+/**
+ * Sample Skeleton for 'MainWindow.fxml' Controller Class
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class MainWindow {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+
+}
