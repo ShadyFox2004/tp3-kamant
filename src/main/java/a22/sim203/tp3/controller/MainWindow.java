f003f354770782f3fa55bcef6f4ace4659475614
ShadyFox2004 <amboudreau.amb@gmail.com>
2022-12-04 00:53:21 -0500

    POPIN ALSO !!!

--- src/main/java/a22/sim203/tp3/controller/MainWindow.java
+++ src/main/java/a22/sim203/tp3/controller/MainWindow.java
@@ -7,10 +7,11 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,9 +31,14 @@ public class MainWindow {
     private History history;
 
     private List<Stage> stages = new ArrayList<>();
+    @FXML
+    private MenuItem popinButton;
+    @FXML
+    private MenuItem popoutButton;
 
     @FXML
     private TabPane tabs;
+
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() throws IOException {
@@ -43,31 +49,77 @@ public class MainWindow {
         tabs.getTabs().add(new Tab("editor" ,editor));
         tabs.getTabs().add(new Tab("simulator", simulator));
         tabs.getTabs().add(new Tab("calculator" ,calculator));
+
+        updatePopItems();
     }
+
     /**
-     * Pop out the tabs into windows
+     * Pop out the tabs into stages
      * // TODO make this cleaner
      */
     @FXML
     private void popout() {
+        popoutButton.setDisable(true);
         tabs.getTabs().forEach(tab -> {
-            Node content = tab.getContent();
-            tab.setContent(null); // Cannot have two view;
-            Stage stage = createStageWithParent((Parent) content);
-            stages.add(stage);
-            stage.show();
+            tabToStage(tab);
         });
         tabs.getTabs().clear();
+        updatePopItems();
+    }
+
+    /**
+     * Pop in the stage into tabs
+     */
+    @FXML
+    private void popin() {
+        popinButton.setDisable(true);
+        stages.forEach(stage -> {
+            stageToTab(stage);
+        });
+        updatePopItems();
     }
 
     /**
      * Create a stage with a root
      */
-    private Stage createStageWithParent(Parent parent) {
-        if (parent == null) return null; // TODO Clean this atrocity
+    private Stage tabToStage(Tab tab) {
+        if (tab == null) return null; // TODO Clean this atrocity
+        Node content = tab.getContent();
+        tab.setContent(null); // Cannot have two view;
         Stage stage = new Stage();
-        stage.setScene(new Scene(parent));
+        stage.setScene(new Scene((Parent) content));
+        stage.setTitle(tab.getText());
+        stages.add(stage);
+        stage.setOnCloseRequest(event -> {
+            stageToTab(stage);
+        });
+        popoutButton.setDisable(tabs.getTabs().size() == 0); // Nice touch for the gui
+        stage.show();
         return stage;
+    }
+
+    /**
+     * Creates a tab from a stage
+     * @param stage the stage that is closing
+     * @return the tab created
+     */
+    private Tab stageToTab(Stage stage) {
+        Tab tab;
+        Parent root = stage.getScene().getRoot();
+        stage.setScene(null);
+        tab = new Tab(stage.getTitle() ,root);
+        tabs.getTabs().add(tab);
+        stage.close();
+         // Nice touch for the gui
+        return tab;
+    }
+
+    /**
+     * Update popin and popout to be disabled when they serve nothing
+     */
+    public void updatePopItems() {
+        popoutButton.setDisable(tabs.getTabs().size() == 0); // Nice touch for the gui
+        popinButton.setDisable(stages.size() == 0);
     }
 
     /**
